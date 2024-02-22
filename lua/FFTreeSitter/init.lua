ae632c27435ab312f39307afd52591c4a0256ef4
MrFlufficans <dc.fluffy.cat@gmail.com>
2024-02-22 10:45:09 +0800

    Improve Treesitter so now it doesn't auto disable itself

--- lua/FFTreeSitter/init.lua
+++ lua/FFTreeSitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"help", "c++", "python", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = {"help", "c++", "javascript" , "python", "c", "lua", "css", "xml", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -19,15 +19,6 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
