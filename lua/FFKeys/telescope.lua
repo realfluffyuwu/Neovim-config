040e22e1d58e7d9a9ce2552d550a995bdbffcae4
Jordon <realfluffyuwu@proton.me>
2024-03-04 00:35:27 +0800

    Changed a whole Bunch of Keys

--- lua/FFKeys/telescope.lua
+++ lua/FFKeys/telescope.lua
@@ -7,29 +7,31 @@ require("telescope").setup {
 	}
 }
 
+local builtin = require("telescope.builtin")
 require("telescope").load_extension "file_browser"
+--local TfileBrowser = require("telescope").extensions.file_browser.file_browser
+
+vim.keymap.set("n", "<leader>sf", builtin.live_grep, {})
+--vim.keymap.set("n", "<leader>sw", builtin.grep_string, {})
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {})
+--vim.keymap.set("n", "<leader>m", TfileBrowser.file_browser, {})
+
+vim.keymap.set("n", "<leader>/", function()
+	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end)
 
 vim.api.nvim_set_keymap(
 	"n",
-	"<space>fs",
-	":Telescope find_files <CR>",
+	"<space>m",
+	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
 	{ noremap = true }
 )
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<space>sf",
-	":Telescope find_files <CR>",
-	{ noremap = true }
-)
-
--- File Browser
-vim.api.nvim_set_keymap(
-  "n",
-  "<space><space>",
-  ":Telescope file_browser <CR>",
-  { noremap = true }
-)
 --local builtin = require("telescope.builtin")
 --vim.keymap.set("n", "<leader>fs", builtin.find_files, {})
 --vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
