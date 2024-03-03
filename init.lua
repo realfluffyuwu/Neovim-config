16bcb0d6bbb88202e23017a615b07db9b9bf0b97
Jordon <realfluffyuwu@proton.me>
2024-03-04 00:35:11 +0800

    I don't use this binding anymore

--- init.lua
+++ init.lua
@@ -4,4 +4,4 @@ require("FFKeys")   -- Pretty well handles all Keybindings except LSP
 require("FFLsp")    -- Handles all the LSP Settings and Keybinds
 
 -- Have to place this here because Nvim is too Retarded to source it properly
-vim.keymap.set("n", "<leader>m", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>m", vim.cmd.Ex)
