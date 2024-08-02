bc212502f64175c247fa68f10a41a5b1cf4217d9
Jordon <realfluffyuwu@proton.me>
2024-08-02 14:30:12 +0800

    Disabled Neo

--- lua/FFKeys/git.lua
+++ lua/FFKeys/git.lua
@@ -1,3 +1,3 @@
 --vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>g<Left>", ":Neotree git_status left<CR>")
-vim.keymap.set("n", "<leader>g<Right>", ":Neotree git_status right<CR>")
+--vim.keymap.set("n", "<leader>g<Left>", ":Neotree git_status left<CR>")
+--vim.keymap.set("n", "<leader>g<Right>", ":Neotree git_status right<CR>")
