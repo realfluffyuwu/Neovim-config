1b004ddbe48d5047c69c9ac3c09788a4edd29c0c
Jordon <realfluffyuwu@proton.me>
2024-08-04 08:05:40 +0800

    Added FFSetup

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 require("FFPacker") -- Packer Plugins
+require("FFSetup")  -- Loads all the Startups for my Plugins
 require("FFTheme")  -- Handles Theme Things
 require("FFKeys")   -- Pretty well handles all Keybindings except LSP
 require("FFLsp")    -- Handles all the LSP Settings and Keybinds
---require("FFSetup")  -- Handles all the Plugin Setup
-
--- Have to place this here because Nvim is too Retarded to source it properly
---vim.keymap.set("n", "<leader>m", vim.cmd.Ex)
